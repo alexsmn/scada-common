e53b606a9f58ac8b891d41c16d8962f2069df4d7
alexsmn <alexsmn@gmail.com>
2018-09-24 16:33:32 -0600

    OPC UA: Server.

--- opcua/opcua_server.cpp
+++ opcua/opcua_server.cpp
@@ -7,34 +7,61 @@
 #include "core/view_service.h"
 #include "opcua/opcua_conversion.h"
 
-#include <algorithm>
-#include <atomic>
-#include <chrono>
 #include <opcuapp/data_value.h>
 #include <opcuapp/requests.h>
 #include <opcuapp/status_code.h>
+#include <algorithm>
+#include <atomic>
+#include <chrono>
 
 namespace {
+
+opcua::ProxyStubConfiguration MakeProxyStubConfiguration() {
+  opcua::ProxyStubConfiguration result;
+  result.bProxyStub_Trace_Enabled = OpcUa_True;
+  result.uProxyStub_Trace_Level = OPCUA_TRACE_OUTPUT_LEVEL_WARNING;
+  result.iSerializer_MaxAlloc = -1;
+  result.iSerializer_MaxStringLength = -1;
+  result.iSerializer_MaxByteStringLength = -1;
+  result.iSerializer_MaxArrayLength = -1;
+  result.iSerializer_MaxMessageSize = -1;
+  result.iSerializer_MaxRecursionDepth = -1;
+  result.bSecureListener_ThreadPool_Enabled = OpcUa_False;
+  result.iSecureListener_ThreadPool_MinThreads = -1;
+  result.iSecureListener_ThreadPool_MaxThreads = -1;
+  result.iSecureListener_ThreadPool_MaxJobs = -1;
+  result.bSecureListener_ThreadPool_BlockOnAdd = OpcUa_True;
+  result.uSecureListener_ThreadPool_Timeout = OPCUA_INFINITE;
+  result.bTcpListener_ClientThreadsEnabled = OpcUa_False;
+  result.iTcpListener_DefaultChunkSize = -1;
+  result.iTcpConnection_DefaultChunkSize = -1;
+  result.iTcpTransport_MaxMessageLength = -1;
+  result.iTcpTransport_MaxChunkCount = -1;
+  result.bTcpStream_ExpectWriteToBlock = OpcUa_True;
+  return result;
+}
 
 void SetNodeClass(const scada::DataValue& data_value, OpcUa_NodeClass& result) {
   if (scada::IsGood(data_value.status_code))
     result = static_cast<OpcUa_NodeClass>(data_value.value.get<int32_t>());
 }
 
-template<class T, class R>
+template <class T, class R>
 void SetValue(const scada::DataValue& data_value, R& result) {
   if (scada::IsGood(data_value.status_code))
     Convert(data_value.value.get<T>(), result);
 }
 
-void FillBrowseResultsTypeDefinitions(scada::ViewService& view_service,
+void FillBrowseResultsTypeDefinitions(
+    scada::ViewService& view_service,
     opcua::Span<const OpcUa_BrowseDescription> descriptions,
-    opcua::Span<OpcUa_BrowseResult> results, const std::function<void()>& callback) {
+    opcua::Span<OpcUa_BrowseResult> results,
+    const std::function<void()>& callback) {
   assert(descriptions.size() == results.size());
 
   std::vector<scada::BrowseDescription> nodes;
   nodes.reserve(descriptions.size());
-  
+
   for (size_t i = 0; i < descriptions.size(); ++i) {
     auto& description = descriptions[i];
     if (!(description.ResultMask & OpcUa_BrowseResultMask_TypeDefinition))
@@ -44,24 +71,23 @@ void FillBrowseResultsTypeDefinitions(scada::ViewService& view_service,
     if (!opcua::StatusCode{result.StatusCode})
       continue;
 
-    for (auto& reference : opcua::MakeSpan(result.References, static_cast<size_t>(result.NoOfReferences))) {
+    for (auto& reference : opcua::MakeSpan(
+             result.References, static_cast<size_t>(result.NoOfReferences))) {
       auto node_id = Convert(reference.NodeId).node_id();
       assert(!node_id.is_null());
       nodes.push_back(scada::BrowseDescription{
-          std::move(node_id),
-          scada::BrowseDirection::Forward,
-          OpcUaId_HasTypeDefinition,
-          false
-      });
+          std::move(node_id), scada::BrowseDirection::Forward,
+          OpcUaId_HasTypeDefinition, false});
     }
   }
 
   if (nodes.empty())
     return callback();
 
-  view_service.Browse(nodes,
-      [descriptions, results, callback]
-      (const scada::Status& status, std::vector<scada::BrowseResult> browse_results) {
+  view_service.Browse(
+      nodes, [descriptions, results, callback](
+                 const scada::Status& status,
+                 std::vector<scada::BrowseResult> browse_results) {
         if (!status)
           return callback();
 
@@ -75,7 +101,9 @@ void FillBrowseResultsTypeDefinitions(scada::ViewService& view_service,
           if (!opcua::StatusCode{result.StatusCode})
             continue;
 
-          for (auto& reference : opcua::MakeSpan(result.References, static_cast<size_t>(result.NoOfReferences))) {
+          for (auto& reference :
+               opcua::MakeSpan(result.References,
+                               static_cast<size_t>(result.NoOfReferences))) {
             auto& browse_result = browse_results[browse_index++];
             if (!scada::IsGood(browse_result.status_code))
               continue;
@@ -83,10 +111,14 @@ void FillBrowseResultsTypeDefinitions(scada::ViewService& view_service,
               continue;
 
             assert(browse_result.references[0].forward);
-            assert(browse_result.references[0].reference_type_id == OpcUaId_HasTypeDefinition);
+            assert(browse_result.references[0].reference_type_id ==
+                   OpcUaId_HasTypeDefinition);
             auto& type_definition_id = browse_result.references[0].node_id;
             assert(!type_definition_id.is_null());
-            Convert(scada::ExpandedNodeId{browse_result.references[0].node_id, {}, 0}, reference.TypeDefinition);
+            Convert(
+                scada::ExpandedNodeId{
+                    browse_result.references[0].node_id, {}, 0},
+                reference.TypeDefinition);
           }
         }
 
@@ -94,9 +126,11 @@ void FillBrowseResultsTypeDefinitions(scada::ViewService& view_service,
       });
 }
 
-void FillBrowseResultsAttributes(scada::AttributeService& attribute_service,
+void FillBrowseResultsAttributes(
+    scada::AttributeService& attribute_service,
     opcua::Span<const OpcUa_BrowseDescription> descriptions,
-    opcua::Span<OpcUa_BrowseResult> results, const std::function<void()>& callback) {
+    opcua::Span<OpcUa_BrowseResult> results,
+    const std::function<void()>& callback) {
   assert(descriptions.size() == results.size());
 
   std::vector<scada::ReadValueId> read_ids;
@@ -108,7 +142,8 @@ void FillBrowseResultsAttributes(scada::AttributeService& attribute_service,
     if (!opcua::StatusCode{result.StatusCode})
       continue;
 
-    for (auto& reference : opcua::MakeSpan(result.References, static_cast<size_t>(result.NoOfReferences))) {
+    for (auto& reference : opcua::MakeSpan(
+             result.References, static_cast<size_t>(result.NoOfReferences))) {
       auto node_id = Convert(reference.NodeId).node_id();
       assert(!node_id.is_null());
       if (description.ResultMask & OpcUa_BrowseResultMask_NodeClass)
@@ -123,57 +158,61 @@ void FillBrowseResultsAttributes(scada::AttributeService& attribute_service,
   if (read_ids.empty())
     return callback();
 
-  attribute_service.Read(read_ids,
-      [descriptions, results, callback]
-      (const scada::Status& status, std::vector<scada::DataValue> values) {
-        if (!status)
-          return callback();
+  attribute_service.Read(read_ids, [descriptions, results, callback](
+                                       const scada::Status& status,
+                                       std::vector<scada::DataValue> values) {
+    if (!status)
+      return callback();
 
-        size_t index = 0;
-        for (size_t i = 0; i < descriptions.size(); ++i) {
-          auto& description = descriptions[i];
-          auto& result = results[i];
-          if (!opcua::StatusCode{result.StatusCode})
-            continue;
+    size_t index = 0;
+    for (size_t i = 0; i < descriptions.size(); ++i) {
+      auto& description = descriptions[i];
+      auto& result = results[i];
+      if (!opcua::StatusCode{result.StatusCode})
+        continue;
 
-          for (auto& reference : opcua::MakeSpan(result.References, static_cast<size_t>(result.NoOfReferences))) {
-            if (description.ResultMask & OpcUa_BrowseResultMask_NodeClass)
-              SetNodeClass(values[index++], reference.NodeClass);
-            if (description.ResultMask & OpcUa_BrowseResultMask_BrowseName)
-              SetValue<scada::QualifiedName>(values[index++], reference.BrowseName);
-            if (description.ResultMask & OpcUa_BrowseResultMask_DisplayName)
-              SetValue<scada::LocalizedText>(values[index++], reference.DisplayName);
-          }
-        }
+      for (auto& reference : opcua::MakeSpan(
+               result.References, static_cast<size_t>(result.NoOfReferences))) {
+        if (description.ResultMask & OpcUa_BrowseResultMask_NodeClass)
+          SetNodeClass(values[index++], reference.NodeClass);
+        if (description.ResultMask & OpcUa_BrowseResultMask_BrowseName)
+          SetValue<scada::QualifiedName>(values[index++], reference.BrowseName);
+        if (description.ResultMask & OpcUa_BrowseResultMask_DisplayName)
+          SetValue<scada::LocalizedText>(values[index++],
+                                         reference.DisplayName);
+      }
+    }
 
-        callback();
-      });
+    callback();
+  });
 }
 
 class MonitoredItemAdapter : public opcua::server::MonitoredItem {
  public:
-  explicit MonitoredItemAdapter(std::unique_ptr<scada::MonitoredItem> monitored_item)
-      : monitored_item_{std::move(monitored_item)} {
-  }
+  explicit MonitoredItemAdapter(
+      std::unique_ptr<scada::MonitoredItem> monitored_item)
+      : monitored_item_{std::move(monitored_item)} {}
 
-  ~MonitoredItemAdapter() {
-  }
+  ~MonitoredItemAdapter() {}
 
   // opcua::server::MonitoredItem
 
-  virtual void SubscribeDataChange(const opcua::server::DataChangeHandler& data_change_handler) override {
-    monitored_item_->set_data_change_handler([this, data_change_handler](const scada::DataValue& data_value) {
-      opcua::DataValue ua_data_value;
-      Convert(scada::DataValue{data_value}, ua_data_value.get());
-      data_change_handler(std::move(ua_data_value));
-    });
+  virtual void SubscribeDataChange(
+      const opcua::server::DataChangeHandler& data_change_handler) override {
+    monitored_item_->set_data_change_handler(
+        [this, data_change_handler](const scada::DataValue& data_value) {
+          opcua::DataValue ua_data_value;
+          Convert(scada::DataValue{data_value}, ua_data_value.get());
+          data_change_handler(std::move(ua_data_value));
+        });
     monitored_item_->Subscribe();
   }
 
-  virtual void SubscribeEvents(const opcua::server::EventHandler& event_handler) override {
-    monitored_item_->set_event_handler([this, event_handler](const scada::Status& status, const scada::Event& event) {
-      event_handler();
-    });
+  virtual void SubscribeEvents(
+      const opcua::server::EventHandler& event_handler) override {
+    monitored_item_->set_event_handler(
+        [this, event_handler](const scada::Status& status,
+                              const scada::Event& event) { event_handler(); });
     monitored_item_->Subscribe();
   }
 
@@ -181,37 +220,45 @@ class MonitoredItemAdapter : public opcua::server::MonitoredItem {
   std::unique_ptr<scada::MonitoredItem> monitored_item_;
 };
 
-} // namespace
+}  // namespace
 
 OpcUaServer::OpcUaServer(OpcUaServerContext&& context)
-    : OpcUaServerContext{std::move(context)} {
-  endpoint_.set_read_handler([this](OpcUa_ReadRequest& request, const opcua::server::ReadCallback& callback) {
-    Read(request, callback);
-  });
+    : OpcUaServerContext{std::move(context)},
+      proxy_stub_{platform_, MakeProxyStubConfiguration()} {
+  endpoint_.set_read_handler(
+      [this](OpcUa_ReadRequest& request,
+             const opcua::server::ReadCallback& callback) {
+        Read(request, callback);
+      });
 
-  endpoint_.set_browse_handler([this](OpcUa_BrowseRequest& request, const opcua::server::BrowseCallback& callback) {
-    Browse(request, callback);
-  });
+  endpoint_.set_browse_handler(
+      [this](OpcUa_BrowseRequest& request,
+             const opcua::server::BrowseCallback& callback) {
+        Browse(request, callback);
+      });
 
-  endpoint_.set_create_monitored_item_handler([this](opcua::ReadValueId& read_value_id) {
-    return CreateMonitoredItem(read_value_id);
-  });
+  endpoint_.set_create_monitored_item_handler(
+      [this](opcua::ReadValueId& read_value_id) {
+        return CreateMonitoredItem(read_value_id);
+      });
 
-  opcua::String url = "opc.tcp://localhost:4840";
-  endpoint_.Open(std::move(url), true, server_certificate_.get(), server_private_key_, &pki_config_,
-      {&security_policy_, 1});
+  endpoint_.Open(url_.c_str(), true, server_certificate_.get(),
+                 server_private_key_, &pki_config_, {&security_policy_, 1});
 }
 
-OpcUaServer::~OpcUaServer() {
-}
+OpcUaServer::~OpcUaServer() {}
 
-void OpcUaServer::Read(OpcUa_ReadRequest& request, const opcua::server::ReadCallback& callback) {
+void OpcUaServer::Read(OpcUa_ReadRequest& request,
+                       const opcua::server::ReadCallback& callback) {
   auto timestamps_to_return = request.TimestampsToReturn;
   attribute_service_.Read(
-      ConvertVector<scada::ReadValueId>(opcua::MakeSpan(request.NodesToRead, request.NoOfNodesToRead)),
-      [timestamps_to_return, callback](const scada::Status& status, std::vector<scada::DataValue> values) {
+      ConvertVector<scada::ReadValueId>(
+          opcua::MakeSpan(request.NodesToRead, request.NoOfNodesToRead)),
+      [timestamps_to_return, callback](const scada::Status& status,
+                                       std::vector<scada::DataValue> values) {
         opcua::ReadResponse response;
-        response.ResponseHeader.ServiceResult = MakeStatusCode(status.code()).code();
+        response.ResponseHeader.ServiceResult =
+            MakeStatusCode(status.code()).code();
         opcua::Vector<OpcUa_DataValue> ua_values(values.size());
         for (size_t i = 0; i < ua_values.size(); ++i) {
           auto& ua_value = ua_values[i];
@@ -233,7 +280,8 @@ void OpcUaServer::Read(OpcUa_ReadRequest& request, const opcua::server::ReadCall
       });
 }
 
-void OpcUaServer::Browse(OpcUa_BrowseRequest& request, const opcua::server::BrowseCallback& callback) {
+void OpcUaServer::Browse(OpcUa_BrowseRequest& request,
+                         const opcua::server::BrowseCallback& callback) {
   auto descriptions = std::make_shared<opcua::Vector<OpcUa_BrowseDescription>>(
       opcua::Attach{}, request.NodesToBrowse, request.NoOfNodesToBrowse);
   request.NodesToBrowse = OpcUa_Null;
@@ -241,23 +289,32 @@ void OpcUaServer::Browse(OpcUa_BrowseRequest& request, const opcua::server::Brow
 
   view_service_.Browse(
       ConvertVector<scada::BrowseDescription>(*descriptions),
-      [this, descriptions, callback](const scada::Status& status, std::vector<scada::BrowseResult> results) {
+      [this, descriptions, callback](const scada::Status& status,
+                                     std::vector<scada::BrowseResult> results) {
         if (!status) {
           opcua::BrowseResponse response;
-          response.ResponseHeader.ServiceResult = MakeStatusCode(status.code()).code();
+          response.ResponseHeader.ServiceResult =
+              MakeStatusCode(status.code()).code();
           callback(response);
           return;
         }
 
         auto ua_results = std::make_shared<opcua::Vector<OpcUa_BrowseResult>>(
-            MakeVector<OpcUa_BrowseResult>(opcua::MakeSpan(results.data(), results.size())));
-        FillBrowseResultsTypeDefinitions(view_service_,
-            opcua::Span<const OpcUa_BrowseDescription>(descriptions->data(), descriptions->size()),
-            opcua::Span<OpcUa_BrowseResult>(ua_results->data(), ua_results->size()),
+            MakeVector<OpcUa_BrowseResult>(
+                opcua::MakeSpan(results.data(), results.size())));
+        FillBrowseResultsTypeDefinitions(
+            view_service_,
+            opcua::Span<const OpcUa_BrowseDescription>(descriptions->data(),
+                                                       descriptions->size()),
+            opcua::Span<OpcUa_BrowseResult>(ua_results->data(),
+                                            ua_results->size()),
             [this, descriptions, ua_results, callback] {
-              FillBrowseResultsAttributes(attribute_service_,
-                  opcua::Span<const OpcUa_BrowseDescription>(descriptions->data(), descriptions->size()),
-                  opcua::Span<OpcUa_BrowseResult>(ua_results->data(), ua_results->size()),
+              FillBrowseResultsAttributes(
+                  attribute_service_,
+                  opcua::Span<const OpcUa_BrowseDescription>(
+                      descriptions->data(), descriptions->size()),
+                  opcua::Span<OpcUa_BrowseResult>(ua_results->data(),
+                                                  ua_results->size()),
                   [descriptions, ua_results, callback] {
                     opcua::BrowseResponse response;
                     response.ResponseHeader.ServiceResult = OpcUa_Good;
@@ -265,21 +322,26 @@ void OpcUaServer::Browse(OpcUa_BrowseRequest& request, const opcua::server::Brow
                     response.Results = ua_results->release();
                     callback(response);
                   });
-              });
             });
+      });
 }
 
-opcua::server::CreateMonitoredItemResult OpcUaServer::CreateMonitoredItem(opcua::ReadValueId& read_value_id) {
+opcua::server::CreateMonitoredItemResult OpcUaServer::CreateMonitoredItem(
+    opcua::ReadValueId& read_value_id) {
   auto id = Convert(read_value_id);
   auto monitored_item = monitored_item_service_.CreateMonitoredItem(id);
   if (!monitored_item)
     return {OpcUa_Bad};
-  return {OpcUa_Good, std::make_unique<MonitoredItemAdapter>(std::move(monitored_item))};
+  return {OpcUa_Good,
+          std::make_unique<MonitoredItemAdapter>(std::move(monitored_item))};
 }
 
-std::chrono::milliseconds GetDateTimeDiff(OpcUa_DateTime a_Value1, OpcUa_DateTime a_Value2) {
-  auto ullValue1 = (static_cast<int64_t>(a_Value1.dwHighDateTime) << 32) + a_Value1.dwLowDateTime;
-  auto ullValue2 = (static_cast<int64_t>(a_Value2.dwHighDateTime) << 32) + a_Value2.dwLowDateTime;
+std::chrono::milliseconds GetDateTimeDiff(OpcUa_DateTime a_Value1,
+                                          OpcUa_DateTime a_Value2) {
+  auto ullValue1 = (static_cast<int64_t>(a_Value1.dwHighDateTime) << 32) +
+                   a_Value1.dwLowDateTime;
+  auto ullValue2 = (static_cast<int64_t>(a_Value2.dwHighDateTime) << 32) +
+                   a_Value2.dwLowDateTime;
   auto ullResult = ullValue2 - ullValue1;
-  return std::chrono::milliseconds{ ullResult * 10 };
+  return std::chrono::milliseconds{ullResult * 10};
 }
--- opcua/opcua_server.h
+++ opcua/opcua_server.h
@@ -5,22 +5,25 @@
 
 #include <opcua_serverapi.h>
 #include <opcuapp/basic_types.h>
+#include <opcuapp/platform.h>
+#include <opcuapp/proxy_stub.h>
 #include <opcuapp/server/endpoint.h>
 #include <opcuapp/structs.h>
 #include <opcuapp/vector.h>
-#include <vector>
 #include <map>
+#include <vector>
 
 namespace scada {
 class AttributeService;
 class MonitoredItemService;
 class ViewService;
-}
+}  // namespace scada
 
 struct OpcUaServerContext {
   scada::AttributeService& attribute_service_;
   scada::ViewService& view_service_;
   scada::MonitoredItemService& monitored_item_service_;
+  const std::string url_;
 };
 
 class OpcUaServer : private OpcUaServerContext {
@@ -29,13 +32,21 @@ class OpcUaServer : private OpcUaServerContext {
   ~OpcUaServer();
 
  private:
-  void Read(OpcUa_ReadRequest& request, const opcua::server::ReadCallback& callback);
-  void Browse(OpcUa_BrowseRequest& request, const opcua::server::BrowseCallback& callback);
-  opcua::server::CreateMonitoredItemResult CreateMonitoredItem(opcua::ReadValueId& read_value_id);
+  void Read(OpcUa_ReadRequest& request,
+            const opcua::server::ReadCallback& callback);
+  void Browse(OpcUa_BrowseRequest& request,
+              const opcua::server::BrowseCallback& callback);
+  opcua::server::CreateMonitoredItemResult CreateMonitoredItem(
+      opcua::ReadValueId& read_value_id);
+
+  opcua::Platform platform_;
+  opcua::ProxyStub proxy_stub_;
 
   const opcua::ByteString server_certificate_;
-  const OpcUa_Key server_private_key_{OpcUa_Crypto_KeyType_Invalid, {0, (OpcUa_Byte*)""}};
-  const OpcUa_P_OpenSSL_CertificateStore_Config pki_config_{OpcUa_NO_PKI, OpcUa_Null, OpcUa_Null,OpcUa_Null, 0, OpcUa_Null};
+  const OpcUa_Key server_private_key_{OpcUa_Crypto_KeyType_Invalid,
+                                      {0, (OpcUa_Byte*)""}};
+  const OpcUa_P_OpenSSL_CertificateStore_Config pki_config_{
+      OpcUa_NO_PKI, OpcUa_Null, OpcUa_Null, OpcUa_Null, 0, OpcUa_Null};
   const opcua::server::Endpoint::SecurityPolicyConfiguration security_policy_;
   opcua::server::Endpoint endpoint_{OpcUa_Endpoint_SerializerType_Binary};
 };
