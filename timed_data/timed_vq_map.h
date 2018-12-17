a08e8c9e48903c0763681141f6a22e1e62823e61
alexsmn <alexsmn@gmail.com>
2018-12-17 14:35:25 -0700

    scada::DataValue span routines.

--- timed_data/timed_vq_map.h
+++ timed_data/timed_vq_map.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "base/span.h"
 #include "core/data_value.h"
 
 #include <algorithm>
@@ -9,30 +10,42 @@ namespace rt {
 
 using DataValues = std::vector<scada::DataValue>;
 
-inline DataValues::iterator LowerBound(DataValues& values, base::Time time) {
+inline std::size_t LowerBound(span<const scada::DataValue> values,
+                              base::Time time) {
   assert(!time.is_null());
-  return std::lower_bound(values.begin(), values.end(), time,
-                          [](const scada::DataValue& value, base::Time time) {
-                            return value.source_timestamp < time;
-                          });
+  auto i = std::lower_bound(values.begin(), values.end(), time,
+                            [](const scada::DataValue& value, base::Time time) {
+                              return value.source_timestamp < time;
+                            });
+  return i - values.begin();
 }
 
-inline DataValues::iterator UpperBound(DataValues& values, base::Time time) {
+inline std::size_t UpperBound(span<const scada::DataValue> values,
+                              base::Time time) {
   assert(!time.is_null());
-  return std::upper_bound(values.begin(), values.end(), time,
-                          [](base::Time time, const scada::DataValue& value) {
-                            return time < value.source_timestamp;
-                          });
+  auto i = std::upper_bound(values.begin(), values.end(), time,
+                            [](base::Time time, const scada::DataValue& value) {
+                              return time < value.source_timestamp;
+                            });
+  return i - values.begin();
 }
 
 inline DataValues::const_iterator LowerBound(const DataValues& values,
                                              base::Time time) {
-  return LowerBound(const_cast<DataValues&>(values), time);
+  return values.begin() + LowerBound(span{values.data(), values.size()}, time);
 }
 
 inline DataValues::const_iterator UpperBound(const DataValues& values,
                                              base::Time time) {
-  return UpperBound(const_cast<DataValues&>(values), time);
+  return values.begin() + UpperBound(span{values.data(), values.size()}, time);
+}
+
+inline DataValues::iterator LowerBound(DataValues& values, base::Time time) {
+  return values.begin() + LowerBound(span{values.data(), values.size()}, time);
+}
+
+inline DataValues::iterator UpperBound(DataValues& values, base::Time time) {
+  return values.begin() + UpperBound(span{values.data(), values.size()}, time);
 }
 
 }  // namespace rt
