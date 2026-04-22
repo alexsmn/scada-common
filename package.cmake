7edab63aa707ad81a7985afee68ad63b311ca005
alexsmn <alexsmn@gmail.com>
2026-04-22 14:47:06 -0700

    Add packaging metadata contract

--- package.cmake
+++ package.cmake
@@ -3,5 +3,8 @@ scada_register_package(
   REPO common
   COMPONENT Common
   DISPLAY_NAME "Общие компоненты"
+  VERSION "${SCADA_PACKAGING_VERSION}"
   DESCRIPTION "Shared runtime package"
+  INSTALL_DESTINATIONS "bin"
+  RUNTIME_DEPENDENCY_POLICY "repo-local-runtime-deps"
   DEPENDS scada-core)
