e930a2ae1af395c0a729a79523a4f04e3084c69c
alexsmn <alexsmn@gmail.com>
2026-04-22 17:45:18 -0700

    Add package distribution metadata

--- package.cmake
+++ package.cmake
@@ -5,6 +5,8 @@ scada_register_package(
   DISPLAY_NAME "Общие компоненты"
   VERSION "${SCADA_PACKAGING_VERSION}"
   DESCRIPTION "Shared runtime package"
+  DISTRIBUTION_KIND "dependency"
+  STANDALONE_INSTALLER_DEFAULT OFF
   INSTALL_DESTINATIONS "bin"
   RUNTIME_DEPENDENCY_POLICY "repo-local-runtime-deps"
   DEPENDS scada-core)
